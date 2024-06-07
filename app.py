3934a45e2c17bc18f388de2925c083dd7182540b
DraconicOv <theredlionzzw@gmail.com>
2024-06-07 10:53:49 -0400

    updating app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-template_dict = {'1':{"Item":'apples','value':2,'request_date':'2024'}}
+template_dict = {'1':{"item":'apples','value':2,'request_date':'2024'},'2':{"item":'potatoes','value':5,'request_date':'2024'}}
 @app.route('/')
 def index():
-    return render_template('index.html', )
+    return render_template('index.html', boxes=template_dict)
 
 
 if __name__ == '__main__':
