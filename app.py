0cbc0f587178d70c6d6c59cabe83f53b1ea1630e
DraconicOv <theredlionzzw@gmail.com>
2024-06-07 11:15:47 -0400

    fixed index.html error, added donator query to app.py

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ template_dict = [{"request_id":1,"item":'apples','value':2,'request_date':'2024'
 
 @app.route('/')
 def index():
-    return render_template('index.html', boxes=template_dict)
-@app.route('/donate', methods=['POST'])
+    return render_template('index.html', boxes=get_all_requests())
+@app.route('/donate')
 def donate():
     if request.method == 'POST':
         request_id = request.form['request_id']
         amount = request.form['amount']
         fulfill_request(request_id, amount)
+    render_template('index.html', boxes=get_all_requests())
     return 'Donation successful'
 if __name__ == '__main__':
     app.run()
