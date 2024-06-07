cf2958b4f2993eb5485fd9f9a1f952564c4079ef
DraconicOv <theredlionzzw@gmail.com>
2024-06-07 12:27:53 -0400

    dsafdsf

--- database.py
+++ database.py
@@ -1,6 +1,13 @@
 import sqlite3
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 database = sqlite3.connect('/home/mintyfresh/repos/FindYourFood/tinkerforchange/foodbank',check_same_thread=False)
+database.row_factory = dict_factory
 cursor = database.cursor()
 
 
@@ -22,20 +29,20 @@ def get_all_requests():
     
 def fulfill_request(request_id, amount):
     try:
-        c_amount_result = cursor.execute('SELECT quantity FROM requests WHERE request_id = ?', (request_id,)).fetchone()
+        c_amount_result = cursor.execute('SELECT quantity FROM requests WHERE request_id = ?', (request_id,)).fetchone()['quantity']
         if c_amount_result is not None:
             c_amount = c_amount_result[0]
             if c_amount - amount <= 0:
                 cursor.execute('UPDATE requests SET status = "Fulfilled" WHERE request_id = ?', (request_id,))
-                user_id_result = cursor.execute('SELECT user_id FROM requests WHERE request_id = ?', (request_id,)).fetchone()
+                user_id_result = cursor.execute('SELECT user_id FROM requests WHERE request_id = ?', (request_id,)).fetchone()['user_id']
                 if user_id_result is not None:
                     user_id = user_id_result[0]
-                    email_result = cursor.execute('SELECT email FROM users WHERE user_id = ?', (user_id,)).fetchone()
+                    email_result = cursor.execute('SELECT email FROM users WHERE user_id = ?', (user_id,)).fetchone()['email']
                     if email_result is not None:
                         email = email_result[0]
                         send_email(email, 'Your request has been fulfilled')
             else:
-                cursor.execute('UPDATE requests SET quantity = quantity - ? WHERE request_id = ?', (amount, request_id))
+                cursor.execute('UPDATE requests SET quantity = ? WHERE request_id = ?', (c_amount_result-amount, request_id))
             database.commit()
     except Exception as e:
         print(f"Error: {e}")
@@ -44,7 +51,7 @@ def create_user(email,name, address):
     try:
         cursor.execute('INSERT INTO users (email, full_name, address) VALUES (?, ?, ?)', (email, name, address))
         database.commit()
-        return cursor.execute('SELECT user_id FROM users WHERE email = ? AND address = ?', (email,address)).fetchone()[0]
+        return cursor.execute('SELECT user_id FROM users WHERE email = ? AND address = ?', (email,address)).fetchone()['user_id']
     except Exception as e:
         print(f"Error: {e}")
         return False
@@ -54,7 +61,7 @@ def send_email(email, message):
     pass
 
 # CREATE TABLE requests (
-#     request_id INT AUTO_INCREMENT PRIMARY KEY,
+#     request_id INT PRIMARY KEY,
 #     user_id INT,
 #     request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 #     item_requested VARCHAR(255) NOT NULL,
@@ -63,7 +70,7 @@ def send_email(email, message):
 #     FOREIGN KEY (user_id) REFERENCES users(user_id)
 # );
 # CREATE TABLE users (
-#     user_id INT AUTO_INCREMENT PRIMARY KEY,
+#     user_id INT PRIMARY KEY,
 #     full_name VARCHAR(255) NOT NULL,
 #     email VARCHAR(255),
 #     address TEXT,
